d096ce21ed2b831558f5c0351ee1f01c778c8a4e
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-06-05 09:08:56 -0500

    feat: :memo: HUNTERS BILLING SYSTEM
    Se le agrega funcionalidad a los botones de agregar y quitar cantidad de productos
    
    by: Jhon Hernandez

--- js/my-body.js
+++ js/my-body.js
@@ -1,20 +1,43 @@
-/**
- * todo: Miguel del futuro arregle esto ▼ 
- * ? Esta una Mier.....da :)
- * @var styles
- */
-import styles from "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" assert { type: "css" };
 export class myBody extends HTMLElement{
-    constructor(){
+    constructor() {
         super();
+        const link = document.createElement("link");
+        link.rel = "stylesheet";
+        link.href = "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css";
+        document.head.appendChild(link);
     }
+
     async components(){
         return await (await fetch("view/my-body.html")).text();
     }
+
+    selection(e){
+        let $ = e.target;
+        if($.nodeName == "BUTTON") {
+            let inputss = this.querySelectorAll(`#${$.dataset.row} input`);
+            if($.innerHTML == "-"){
+                inputss.forEach(element => {
+                    if(element.getAttribute("name") == "amount" && element.value == 0){
+                        this.querySelector(`#${$.dataset.row}`).remove();
+                    } else if(element.name == "amount"){
+                        element.value--;
+                    }
+                });
+            } else{
+                inputss.forEach(element => {
+                    if(element.getAttribute("name") == "amount"){
+                        element.value++;
+                    }
+                });
+            }
+        }
+    }
+
     connectedCallback(){
-        document.adoptedStyleSheets.push(styles);
         this.components().then(html=>{
             this.innerHTML = html;
+            this.container = this.querySelector("#products");
+            this.container.addEventListener("click", this.selection);
         })
     }
 }
